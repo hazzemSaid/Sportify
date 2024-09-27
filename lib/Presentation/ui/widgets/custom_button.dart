60a97a0c8f911cc301b21fa6345a11f04a0d2d44
haazemsaid <haazemsaidd@gmail.com>
2024-09-27 16:04:47 +0300

    Refactor CustomButton constructor

--- lib/Presentation/ui/widgets/custom_button.dart
+++ lib/Presentation/ui/widgets/custom_button.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
-
+  final int x = 1;
   const CustomButton(
-      {super.key,
-      required this.text,
-      required this.onPressed}); // تأكد من تهيئة النص والدالة
+      {super.key, required this.text, required this.onPressed}); //Constructor
 
   @override
   Widget build(BuildContext context) {
