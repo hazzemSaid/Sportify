fdda708e20cce03f887762f322fd720a3d9ae057
haazemsaid <haazemsaidd@gmail.com>
2024-09-29 01:15:49 +0300

    Refactor authentication logic using bloc -> cubit pattern in log in

--- lib/features/AuthFeatures/presentation/view/screens/login_screen.dart
+++ lib/features/AuthFeatures/presentation/view/screens/login_screen.dart
@@ -1,11 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:sportify/core/utils/routes/routes.dart';
 import 'package:sportify/features/AuthFeatures/presentation/view/widgets/buildPasswordField.dart';
 import 'package:sportify/features/AuthFeatures/presentation/view/widgets/buildSocialButton.dart';
 import 'package:sportify/features/AuthFeatures/presentation/view/widgets/buildTextField.dart';
+import 'package:sportify/features/AuthFeatures/presentation/viewmodel/auth_bloc/auth_cubit.dart';
 import 'package:sportify/features/Onbording_Feature/Presentation/view/widgets/custom_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -31,161 +32,243 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   // login function for firebase auth
-  login() async {
-    try {
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: email.text, password: password.text);
-      print('User login successfully');
-      Get.toNamed(AppRoutes.onBoarding);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
-      }
-    } catch (e) {
-      print(e.toString());
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff2C2C2C),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        toolbarHeight: 20,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 50),
-                const Text(
-                  "Welcome Back",
-                  style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is SginInLoading) {
+          Get.dialog(
+            const Center(
+              child: CircularProgressIndicator(
+                color: Color(0xff00A3B7),
+              ),
+            ),
+            barrierDismissible: false,
+          );
+        } else if (state is SginInSuccess) {
+          Get.dialog(
+            AlertDialog(
+              backgroundColor: const Color(0xff2C2C2C),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              title: const Row(
+                children: [
+                  Icon(
+                    Icons.check_circle_outline,
+                    color: Colors.green,
+                    size: 30,
                   ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Please login to enjoy full feature",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-                const SizedBox(height: 35),
-                Form(
-                  key: _formLoginKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 15),
-                      buildTextField(
-                        controller: email,
-                        label: 'Email',
-                        hint: 'Enter Email',
-                        icon: Icons.email_outlined,
+                  SizedBox(width: 10),
+                  Text(
+                    'Registration Successful',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    "assets/images/success.gif",
+                    height: 120,
+                    width: 120,
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'You have successfully created an account!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              actions: [
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Get.back();
+                      Get.toNamed(AppRoutes.loginScreen);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xff00A3B7), // Button background color
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(20), // Rounded button
                       ),
-                      const SizedBox(height: 20),
-                      buildPasswordField(
-                        password: password,
-                        isObscured: _isObscured,
-                        onpressedF: () {
-                          setState(() {
-                            _isObscured = !_isObscured;
-                          });
-                        },
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 12),
+                    ),
+                    child: const Text(
+                      'OK',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
                       ),
-                      const SizedBox(height: 15),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              'Forgot your password?',
-                              style: TextStyle(
-                                color: Colors.white,
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.11,
-                      ),
-                      CustomButton(
-                        onPressed: () {
-                          if (_formLoginKey.currentState!.validate()) {
-                            login();
-                          }
-                        },
-                        text: "Login",
-                      ),
-                      const SizedBox(height: 25),
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: Divider(color: Colors.grey, thickness: 1),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              "Or",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          Expanded(
-                            child: Divider(color: Colors.grey, thickness: 1),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 25),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          buildSocialButton(FontAwesomeIcons.google, () {
-                            // Implement Google login
-                          }),
-                          const SizedBox(width: 30),
-                          buildSocialButton(Icons.facebook, () {
-                            // Implement Facebook login
-                          }),
-                        ],
-                      ),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.16,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Already have an account?",
-                            style: TextStyle(fontSize: 15, color: Colors.white),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Get.toNamed(AppRoutes.registerScreen);
-                            },
-                            child: const Text(
-                              "Register",
-                              style: TextStyle(
-                                  fontSize: 16, color: Color(0xff00A3B7)),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ],
+            ),
+            barrierDismissible:
+                false, // Prevent the dialog from closing by tapping outside
+          );
+        } else if (state is SginInFailed) {
+          Get.snackbar('Error', state.error ?? 'An error occurred',
+              snackPosition: SnackPosition.TOP,
+              backgroundColor: Colors.red,
+              colorText: Colors.white);
+        }
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xff2C2C2C),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          toolbarHeight: 20,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 50),
+                  const Text(
+                    "Welcome Back",
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Please login to enjoy full feature",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  const SizedBox(height: 35),
+                  Form(
+                    key: _formLoginKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 15),
+                        buildTextField(
+                          controller: email,
+                          label: 'Email',
+                          hint: 'Enter Email',
+                          icon: Icons.email_outlined,
+                        ),
+                        const SizedBox(height: 20),
+                        buildPasswordField(
+                          password: password,
+                          isObscured: _isObscured,
+                          onpressedF: () {
+                            setState(() {
+                              _isObscured = !_isObscured;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                'Forgot your password?',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.11,
+                        ),
+                        CustomButton(
+                          onPressed: () {
+                            if (_formLoginKey.currentState!.validate()) {
+                              context.read<AuthCubit>().sgininwithemail(
+                                    emailAddress: email.text,
+                                    password: password.text,
+                                  );
+                            }
+                          },
+                          text: "Login",
+                        ),
+                        const SizedBox(height: 25),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Divider(color: Colors.grey, thickness: 1),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Text(
+                                "Or",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Expanded(
+                              child: Divider(color: Colors.grey, thickness: 1),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            buildSocialButton(FontAwesomeIcons.google, () {
+                              // Implement Google login
+                            }),
+                            const SizedBox(width: 30),
+                            buildSocialButton(Icons.facebook, () {
+                              // Implement Facebook login
+                            }),
+                          ],
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.16,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Already have an account?",
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.white),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Get.toNamed(AppRoutes.registerScreen);
+                              },
+                              child: const Text(
+                                "Register",
+                                style: TextStyle(
+                                    fontSize: 16, color: Color(0xff00A3B7)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
